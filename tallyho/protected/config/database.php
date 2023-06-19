5780fad609362becc9b355c5d2cd199232ba885e
rohan <rohannak2057@gmail.com>
2023-06-19 12:00:17 +0530

    first commit

---
+++ tallyho/protected/config/database.php
@@ -0,0 +1,10 @@
+<?php
+
+// This is the database connection configuration.
+return array(
+	// uncomment the following lines to use a MySQL database
+	'connectionString' => 'mysql:host=dbService;port=3306;dbname=tally64g_tallyho',
+	'emulatePrepare' => true,
+	'username' => 'tally64g_tallyho',
+	'password' => 'c!90zog*t^P',
+);
